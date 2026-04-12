de5b883efdf57ad75d388c4a72577898e3af5018
Loas717 <133361796+Loas717@users.noreply.github.com>
2026-04-12 16:54:38 -0300

    Primeiros testes

---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2
+import pickle
+import numpy as np
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt') 
+
+with open('vagas_coordenadas.pkl', 'rb') as f:
+    vagas_pos = pickle.load(f)
+
+video = cv2.VideoCapture('estacionamento_video.mp4')
+
+while True:
+    check, frame = video.read()
+    if not check: break
+
+    results = model.predict(frame, classes=[2, 5, 7], conf=0.4, verbose=False)
+    
+    centros_veiculos = []
+    for r in results:
+        for box in r.boxes.xyxy:
+            x1, y1, x2, y2 = box
+            centro_x = int((x1 + x2) / 2)
+            centro_y = int((y1 + y2) / 2)
+            centros_veiculos.append((centro_x, centro_y))
+
+    vagas_livres = 0
+    for vaga in vagas_pos:
+        vaga_poly = np.array(vaga, np.int32)
+        ocupada = False
+        
+        for cx, cy in centros_veiculos:
+            dist = cv2.pointPolygonTest(vaga_poly, (cx, cy), False)
+            if dist >= 0:
+                ocupada = True
+                break
+        
+        color = (0, 0, 255) if ocupada else (0, 255, 0)
+        if not ocupada: vagas_livres += 1
+        
+        cv2.polylines(frame, [vaga_poly], True, color, 2)
+
+    cv2.putText(frame, f'Vagas Livres: {vagas_livres}', (30, 50), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+    cv2.imshow("Detecção com YOLOv8", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
---
+++ selector.py
@@ -0,0 +1,33 @@
+import cv2
+import pickle
+import numpy as np
+
+vagas = []
+vaga_atual = []
+
+def mouse_click(events, x, y, flags, params):
+    global vaga_atual
+    if events == cv2.EVENT_LBUTTONDOWN:
+        vaga_atual.append((x, y))
+        if len(vaga_atual) == 4:
+            vagas.append(vaga_atual)
+            vaga_atual = []
+    
+    if events == cv2.EVENT_RBUTTONDOWN:
+        with open('vagas_coordenadas.pkl', 'wb') as f:
+            pickle.dump(vagas, f)
+        print("Vagas salvas!")
+
+img = cv2.imread('estacionamento_base.png')
+
+while True:
+    img_display = img.copy()
+    for vaga in vagas:
+        cv2.polylines(img_display, [np.array(vaga)], True, (0, 255, 0), 2)
+    
+    cv2.imshow("Clique em 4 pontos por vaga | Botão Direito p/ Salvar", img_display)
+    cv2.setMouseCallback("Clique em 4 pontos por vaga | Botão Direito p/ Salvar", mouse_click)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
